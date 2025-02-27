1116b8f38432ec8d2873492047ba3aede3263e34
Chetna-Jha-QC <ms.chetnajha@gmail.com>
2025-02-27 15:59:32 +0530

    adding Ticket.js and User.js models

---
+++ models/Ticket.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema({
+    userId:              { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    dateOfTravel:        { type: Date, required: true },
+    modeOfTravel:        { type: String, enum: ["rail", "bus"], required: true },
+    perHeadPrice:        { type: Number, required: true },
+    from:                { type: String, required: true },
+    to:                  { type: String, required: true },
+    numberOfPassengers:  { type: Number, required: true },
+    totalPrice:          { type: Number, required: true }
+});
+
+//to auto calculate the price
+ticketSchema.pre('save', function (next ){
+    this.totalPrice = this.perHeadPrice* this.numberOfPassengers;
+    next();
+});
+
+const Ticket = mongoose.model('ticket', ticketSchema);
+
+
+module.exports = Ticket;
